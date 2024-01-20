44aa17b0c16bc0b82ae3b22d6e525636bddfe8de
andrea <andrea.thouvenot@le-campus-numerique.fr>
2024-01-20 19:41:36 +0100

    style pour nom de contact

--- styleContactSelect.php
+++ styleContactSelect.php
@@ -1,25 +1,28 @@
 <?php header("Content-type: text/css; charset=UTF-8"); ?>
 
 
-select{
+select,input{
+    color :black;
     background-color: darkgrey;
     border: none;
     border-radius:5px;
     font-size: 16px;
     font-family: 'Courier New', Courier, monospace;
     box-shadow: 0px 2px 2px 0px lightskyblue;
-    color :black;
-    }
+
+}
 form div{
-width: 100%;
-display: flex;
-flex-direction: column;
-min-height: 83px;
-margin-top: 25px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    min-height: 83px;
+    margin-top: 25px;
 }
 
-<!--select > option{-->
-<!--color:black;}-->
+#nom::placeholder{
+color: black;
+font-style: italic;
+}
 
 
 
