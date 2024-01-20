d23bb8b3fc0b8bf354aca2ac03e12f11fe915d5b
andrea <andrea.thouvenot@le-campus-numerique.fr>
2024-01-20 18:22:41 +0100

    contactRefait( le champ civilité)

--- contactRefait.php
+++ contactRefait.php
@@ -1,14 +1,15 @@
 <!DOCTYPE>
 <html lang="fr">
-<?php include ('header.php'); ?>
+<link rel="stylesheet" href="styleContactSelect.php" />
+<?php //include ('header.php'); ?>
 <body>
 <main>
     <form action="recupereInfoContact.php" method="post">
 <!--        Un champ civilité <select>, M. et Mme-->
         <div>
             <label for="civility">Quel est votre civilité ? </label>
-            <select name="civility" id="civility" placeholder="Votre civilité" >
-                <option value="" disabled selected hidden> Choisissez votre civilité</option>
+            <select name="civility" id="civility"  >
+                <option value="" disabled selected hidden class="title_selection"  > Choisissez votre civilité</option>
                 <option value="Vous êtes une femme">Mme</option>
                 <option value="Vous êtes un homme">M.</option>
                 <option value="Vous êtes ni homme ni femme">Autres</option>
