011a36be02c47d74b5a52a56a7f039c12d7e5eaa
andrea <andrea.thouvenot@le-campus-numerique.fr>
2024-01-21 00:24:47 +0100

    include le header

--- contactRefait.php
+++ contactRefait.php
@@ -1,25 +1,7 @@
 <!DOCTYPE>
 <html lang="fr">
-<head>
-    <meta charset="utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
-<!--    <description> --><?php //echo $MetaDescription ?><!-- </description>-->
-    <link rel="stylesheet" href="styleContactSelect.php" />
-    <!--    <link rel="stylesheet" href="style.php" />-->
-    <title> contact<?php // echo $MetaTitle ?></title>
-</head>
+<?php include ('header.php') ; ?>
 <body>
-    <header>
-        <div>
-            <a href="cv.php">Mon CV</a>
-        </div>
-        <div>
-            <a href="hobby.php">Mon Hobby</a>
-        </div>
-        <div>
-            <a href="contact.php">Contact</a>
-        </div>
-    </header>
     <main class="contact_main">
         <div class="contact">
             <form action="https://httpbin.org/post" method="post">
