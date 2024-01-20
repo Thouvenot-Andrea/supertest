a319494cb0056dd00b4040bd6ac3ba8d2accb430
andrea <andrea.thouvenot@le-campus-numerique.fr>
2024-01-20 18:23:47 +0100

    style pour civility de contact

---
+++ styleContactSelect.php
@@ -0,0 +1,25 @@
+<?php header("Content-type: text/css; charset=UTF-8"); ?>
+
+
+select{
+    background-color: darkgrey;
+    border: none;
+    border-radius:5px;
+    font-size: 16px;
+    font-family: 'Courier New', Courier, monospace;
+    box-shadow: 0px 2px 2px 0px lightskyblue;
+    color :black;
+    }
+form div{
+width: 100%;
+display: flex;
+flex-direction: column;
+min-height: 83px;
+margin-top: 25px;
+}
+
+<!--select > option{-->
+<!--color:black;}-->
+
+
+
