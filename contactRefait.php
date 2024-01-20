99d91d1192c3198f31ee9fe4281f4c8439a080e6
andrea <andrea.thouvenot@le-campus-numerique.fr>
2024-01-20 16:19:20 +0100

    contactRefait( le champ civilité)

---
+++ contactRefait.php
@@ -0,0 +1,24 @@
+<!DOCTYPE>
+<html lang="fr">
+<?php include ('header.php'); ?>
+<body>
+<main>
+    <form action="recupereInfoContact.php" method="post">
+<!--        Un champ civilité <select>, M. et Mme-->
+        <div>
+            <label for="civility">Quel est votre civilité ? </label>
+            <select name="civility" id="civility" placeholder="Votre civilité" >
+                <option value="" disabled selected hidden> Choisissez votre civilité</option>
+                <option value="Vous êtes une femme">Mme</option>
+                <option value="Vous êtes un homme">M.</option>
+                <option value="Vous êtes ni homme ni femme">Autres</option>
+            </select>
+        </div>
+
+    </form>
+
+</main>
+
+</body>
+
+</html>
