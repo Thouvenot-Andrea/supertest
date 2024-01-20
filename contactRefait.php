af79e0e131d422b5ff65f4460bb9b428ee83de0b
andrea <andrea.thouvenot@le-campus-numerique.fr>
2024-01-20 18:23:18 +0100

    contactRefait( le champ civilité)

--- contactRefait.php
+++ contactRefait.php
@@ -9,7 +9,7 @@
         <div>
             <label for="civility">Quel est votre civilité ? </label>
             <select name="civility" id="civility"  >
-                <option value="" disabled selected hidden class="title_selection"  > Choisissez votre civilité</option>
+                <option value="" disabled selected hidden> Choisissez votre civilité</option>
                 <option value="Vous êtes une femme">Mme</option>
                 <option value="Vous êtes un homme">M.</option>
                 <option value="Vous êtes ni homme ni femme">Autres</option>
