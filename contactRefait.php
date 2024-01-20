44a29bb536779d78ece0caaa11b7b36b3f17e598
andrea <andrea.thouvenot@le-campus-numerique.fr>
2024-01-20 19:53:12 +0100

    contactRefait( le champ email)

--- contactRefait.php
+++ contactRefait.php
@@ -15,13 +15,20 @@
                 <option value="Vous êtes ni homme ni femme">Autres</option>
             </select>
         </div>
+        <!--  Champ pour le nom-->
         <div>
             <label for="nom">Votre Nom</label>
             <input type="text" id="nom" name="nom" placeholder="ex : LEGRAND">
         </div>
+        <!--  Champ pour le prénom -->
         <div>
             <label for="prénom">Votre Prénom</label>
             <input type="text" id="prénom" name="prénom" placeholder="ex : Jean">
+        </div>
+        <!--  Champ pour l'email -->
+        <div>
+            <label for="email"> Votre E-mail</label>
+            <input type="email" id="email" name="email" placeholder=" ex : MonAdress@mail.com">
         </div>
     </form>
 
