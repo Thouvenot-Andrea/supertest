3c47c92f592e01bbda1e16f82509b4842bcc5d8c
andrea <andrea.thouvenot@le-campus-numerique.fr>
2024-01-21 13:52:26 +0100

    footer

--- footer.php
+++ footer.php
@@ -3,6 +3,15 @@
 
     <?php
 
+    // Vérifier si la date de première visite n'est pas encore définie
+    if (!isset($_SESSION['dateFirstVisit'])) {
+        // Enregistrer la date actuelle comme date de première visite
+        $_SESSION['dateFirstVisit'] = date('Y-m-d H:i:s');
+    }
+
+    // Afficher la date de première visite dans le footer
+    echo 'Date de première visite : ' . $_SESSION['dateFirstVisit'];
+
 
     // Définit le fuseau horaire par défaut à utiliser.
     date_default_timezone_set('Europe/Paris');
@@ -12,7 +21,6 @@
      H:i:s');
 
     // Affichage de quelque chose comme : Monday 8th Pf August 2005 03:12:46 PM
-    echo $dateFirstVisit;
     //echo date('l jS \of F Y h:i:s A');
 
     //    // Affichage de quelque chose comme : Monday
@@ -29,8 +37,6 @@
     // Affichage de quelque chose comme : 2000-07-01T00:00:00+00:00
     //    echo date (DATE_ATOM, mktime(0, 0, 0, 7, 1, 2000));
     //
-
-
 
 
 
