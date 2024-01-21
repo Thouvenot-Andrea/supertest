29721a006dcf6805656fe064fe77e3bfdc577878
andrea <andrea.thouvenot@le-campus-numerique.fr>
2024-01-21 16:08:00 +0100

    hobby.php

---
+++ page/hobby.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang ="fr">
+<?php $MetaTitle ='Mon hobby'; $MetaDescription ='Voici mon hobby';?>
+
+<?php include('header.php'); ?>
+<body>
+<div class =" texte-hobby">
+    <div class="titre">Manga</div>
+    <div>
+        <p class = "manga" >
+            Un manga est une bande dessinée japonaise.
+            <br>La plupart des mangas se conforment à un style développé au Japon à la fin du XIX siècle.
+            <br>Au Japon, le terme manga design plus généralement la bande dessinée, quelle que soit son origine.
+            <br>En occident, le mot « manga » design une bande dessinée japonaise, mais aussi parfois une bande dessinée non japonaise respectant les codes des productions populaires japonaises.
+            <br>Les mangas se lisent généralement de droite à gauche.
+            <br>En raison du rythme élevé de parution et pour limiter le coût d'impression, la plupart des mangas sont dessinés en noir et blanc mis à part la couverture.
+            <br>Les deux premiers pays les plus consommateurs de mangas sont le Japon et la France.
+            <br>La personne réalisant des mangas est appelée mangaka.
+        </p>
+        <a href="https://www.senscritique.com/top/resultats/les_meilleurs_mangas/192836"> Les meilleur mangas</a>
+    </div>
+</div>
+<main class="video">
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/h1zlIpvr_bs?si=XAgIVo2EXikuFOHF" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+    <p class="titre-video">La brève histoire du manga</p>
+</main>
+
+
+
+
+</body>
+
+
+<?php include('footer.php'); ?>
+
+</html>
